c6686a4df5a3d6f0eca4d11bbf95588a2397366d
DaniilChudo <daniilpro1994@gmail.com>
2024-11-11 14:02:58 +0100

    add generation of concats

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,24 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...contacts, ...newContacts];
+
+    await writeContacts(updatedContacts);
+
+    console.log(`${number} new contacts have been generated and added.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
+
+generateContacts(10);
