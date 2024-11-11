0ebf03c34a31826c4c00c3bd00df8ed4111c439f
DaniilChudo <daniilpro1994@gmail.com>
2024-11-11 14:56:33 +0100

    remov all contacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,17 @@
-export const removeAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
+export const removeAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length > 0) {
+      await writeContacts([]);
+      console.log('All contacts have been removed.');
+    } else {
+      console.log('No contacts to remove.');
+    }
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 removeAllContacts();
